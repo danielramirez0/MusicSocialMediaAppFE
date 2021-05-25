957ce3b8b6b2680889b079287405b8aff7a9691a
Daniel Ramirez <notused@notused.net>
2021-05-24 18:53:17 -0700

    INIT

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1 className="H1">Hello</h1>
+    </div>
+  );
+}
+
+export default App;

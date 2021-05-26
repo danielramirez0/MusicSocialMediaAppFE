00d023a16fddf1e84d2ee536263d5d683311566c
T U <tylerletendre98@gmail.com>
2021-05-25 22:28:24 -0400

    added in the changes so we can use the router to connect to different pages but commented it out so the page is able to render since we dont have those components yet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import {Switch , Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      {/* <div>
+        <Switch>
+          <Route path='/' exact component={MainPage}/>
+          <Route path='/myProfilePage' component={MyProfilePage}/>
+          <Route path='/otherUserProfilePage' component={OtherUserProfilePage}/>
+          <Route path='/searchUserPage' component={SearchUserPage}/>
+        </Switch>
+      </div> */}
       <header className="App-header"></header>
       <h1 className="H1">Hello</h1>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {BrowserRouter as Router} from 'react-router-dom';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </Router>,
   document.getElementById("root")
 );

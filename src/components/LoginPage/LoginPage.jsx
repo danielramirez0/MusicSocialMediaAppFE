0b511231ada7bad972048fbe03da131a3ea5d11b
Daniel Ramirez <notused@notused.net>
2021-06-01 15:28:43 -0700

    Setting up additional framework for user authentication and proper state control at App() level

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,38 +1,43 @@
+// import jwtDecode from "jwt-decode";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
-import jwtDecode from "jwt-decode";
 import NavBar from "../NavBar/NavBar";
 import useForm from "../useForm/useForm";
+import "./LoginPage.css";
 
 const LoginPage = (props) => {
   const { values, handleChange, handleSubmit } = useForm(login);
+  const history = useHistory();
 
   async function login() {
     await axios
       .post("http://localhost:5000/api/auth", values)
       .then((response) => {
         localStorage.setItem("token", response.data);
+        alert("Logged in");
+        history.push("/myProfilePage");
       })
       .catch((error) => {
         console.log(error);
         alert(error.response.data);
       });
-
-    const token = localStorage.getItem("token");
-    try {
-      const user = jwtDecode(token);
-      // alert("Welcome back!" + user.Name);
-      console.log(user);
-    } catch (error) {
-      console.log(error);
-    }
   }
+  //   const token = localStorage.getItem("token");
+  //   try {
+  //     const user = jwtDecode(token);
+  //     alert(`Welcome back! ${user.firstName}`);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
   return (
     <div className="container">
       <div className="pb-5">
         <NavBar userLoggedIn={false} tabActive="n/a" />
       </div>
-      <div className="pt-4">
-        <h1 className="">User Login</h1>
+      <div className="center center-vertical small-box">
+        <h1 className="text-center">User Login</h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="userEmail" className="form-label">
@@ -44,9 +49,9 @@ const LoginPage = (props) => {
               name="email"
               id="userEmail"
               aria-describedby="emailHelp"
-              value={values.email}
+              value={values.email || ""}
               onChange={handleChange}
-              // required={true}
+              required={true}
             />
           </div>
           <div className="mb-3">
@@ -58,9 +63,9 @@ const LoginPage = (props) => {
               type="password"
               name="password"
               id="userPassword"
-              value={values.password}
+              value={values.password || ""}
               onChange={handleChange}
-              // required={true}
+              required={true}
             />
           </div>
           <div className="mb-3 form-check">

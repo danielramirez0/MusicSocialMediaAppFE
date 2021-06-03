859a25352c88f1a0660b85b34b0e23a01b3ce349
Jaime Alvarez <jrralvarez@gmail.com>
2021-06-03 16:03:08 -0400

    working on adding an image tied with the rest of the user input

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -4,15 +4,59 @@ import NavBar from "../NavBar/NavBar";
 import useForm from "../useForm/useForm";
 import "./RegisterPage.css";
 import { useAppContext } from "../../libs/contextLib";
+import React, {useState} from "react";
 
 const RegisterPage = () => {
   const { errors, values, handleChange, handleSubmit } = useForm(register);
   const { isAuthenticated, userHasAuthenticated, setUser } = useAppContext();
   const history = useHistory();
+  const [fileName, setFileName] = useState('');
+
+  const onChangeFile = (e) => {
+    console.log(e);
+    setFileName(e.target.files[0]);
+  }
+
+ /*  const changeOnClick = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+  
+    formData.append("photoImage", fileName);
+
+    axios
+      .post("http://localhost:5000/api/users/", formData)
+      .then((response) => {
+        localStorage.setItem("token", response.headers["x-auth-token"]);
+        userHasAuthenticated(true);
+        setUser(response.data);
+        history.push("/myProfilePage");
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log(error.response.data);
+    })
+  } */
 
   async function register() {
-    const { confirmPassword, ...newUser } = values;
+    const { confirmPassword, ...newUser } = {...values, photoImage: fileName};
+  
+  
+    //formData.append("Test", 'test');
+/*     formData.append("firstName", newUser.firstName);
+    formData.append("lastName", newUser.lastName);
+    formData.append("email", newUser.email);
+    formData.append("password", newUser.password);
+    formData.append("favoriteArtist", newUser.favoriteArtist);
+    formData.append("favoriteAlbum", newUser.favoriteAlbum);
+    formData.append("favoriteSong", newUser.favoriteSong);
+    formData.append("photoImage", fileName); */
 
+    console.log(fileName);
+    console.log(values);
+
+    console.log(newUser);
+    
     await axios
       .post("http://localhost:5000/api/users/", newUser)
       .then((response) => {
@@ -35,7 +79,7 @@ const RegisterPage = () => {
       <div className="center" id="register-container">
         <div className="center full-box">
           <h1 className="text-center">User Registration</h1>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit} encType="multipart/form-data">
             <div className="input-group mb-3">
               <span className="input-group-text">First Name</span>
               <input
@@ -135,6 +179,14 @@ const RegisterPage = () => {
                 onChange={handleChange}
                 required={true}
               />
+              <div className="form-group">
+                <label>Choose profile picture</label>
+                <input 
+                type="file" 
+                name="photoImage" 
+                className ="form-control-file" 
+                onChange={onChangeFile}/>
+              </div>
             </div>
             <div className="text-center">
               <p>

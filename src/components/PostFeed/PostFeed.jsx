5f91f8ef3b2fa8fe8ba78a1c490cfdee71577bee
Jaime Alvarez <jrralvarez@gmail.com>
2021-06-04 22:35:37 -0500

    able to see the date of the post.

--- src/components/PostFeed/PostFeed.jsx
+++ src/components/PostFeed/PostFeed.jsx
@@ -2,40 +2,44 @@ import React, { useEffect, useState } from "react";
 import "./PostFeed.css";
 import { useAppContext } from "../../libs/contextLib";
 import axios from "axios";
+import dateFormat from "dateformat";
 
 const PostFeed = () => {
-  const { loggedInUser } = useAppContext();
-  const [posts, setPosts] = useState([]);
+	const { loggedInUser } = useAppContext();
+	const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:5000/api/posts/${loggedInUser._id}`)
-      .then((response) => setPosts(response.data));
-  }, [loggedInUser]);
+	useEffect(() => {
+		axios
+			.get(`http://localhost:5000/api/posts/${loggedInUser._id}`)
+			.then((response) => setPosts(response.data));
+	}, [loggedInUser]);
 
-  return posts.length === 0 ? (
-    <div>
-      <h3>There are no post in this feed</h3>
-    </div>
-  ) : (
-    <div className="row">
-      {posts.map((post) => (
-        <div key={post._id} className="post-container">
-          <div className="post-user-pic">
-            <img
-              src="https://www.bing.com/th?id=OIP.N8rKfbKT-MHFneNrhohDKgHaEo&w=155&h=100&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
-              alt=""
-            />
-          </div>
-          <div className="post-body">{post.body}</div>
-          <div>
-            <button>Likes: {post.likes}</button>
-            <button>Dislikes: {post.dislikes}</button>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+	return posts.length === 0 ? (
+		<div>
+			<h3>There are no post in this feed</h3>
+		</div>
+	) : (
+		<div className="row">
+			{posts.map((post) => (
+				<div key={post._id} className="post-container">
+					<div className="post-user-pic ">
+						<img
+							src="https://www.bing.com/th?id=OIP.N8rKfbKT-MHFneNrhohDKgHaEo&w=155&h=100&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
+							alt=""
+						/>
+					</div>
+					<div className="post-date ">
+						{dateFormat(post.date, "mmmm dS, yyyy --- h:MM:ss TT")}
+						<p className="post-body">{post.body}</p>
+					</div>
+					<div className="post-buttons">
+						<button>Likes: {post.likes}</button>
+						<button>Dislikes: {post.dislikes}</button>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default PostFeed;
